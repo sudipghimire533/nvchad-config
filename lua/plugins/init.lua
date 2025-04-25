784863400b9694e4762c88b008e20c132eaa1adf
sudipghimire533 <sudipghimire533@gmail.com>
2025-04-25 07:18:48 +0200

    Update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,6 +28,9 @@ return {
                 "toml",
                 "typescript",
                 "markdown",
+                "go",
+                "gomod",
+                "gosum",
             },
         },
     },
@@ -69,5 +72,21 @@ return {
     -- for typescript
     {
         "typescript-language-server/typescript-language-server",
+    },
+
+    -- let's try go
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
+        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
 }
